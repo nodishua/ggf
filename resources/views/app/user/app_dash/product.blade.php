afca6149373be7ba56710d48328507e537be7e80
unknown <dimaslutfhi82@gmail.com>
2020-07-01 20:50:03 +0700

    adm lg

--- resources/views/app/user/app_dash/product.blade.php
+++ resources/views/app/user/app_dash/product.blade.php
@@ -5,7 +5,7 @@
         @foreach($product as $products)
         <div class="col-md-4 d-flex align-items-stretch">
             <div class="card bb">
-              <img src="{{ url('images/products') }}/{{ $products->image}}" class="card-img-top" alt="...">
+              <img src="{{ url('images/products') }}/{{ $products->image}}" class="card-img-top" alt="..."><hr>
               <div class="card-body">
                 <h5 class="card-title">{{ $products->name_product }}</h5>
                 <p class="card-text">

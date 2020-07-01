f4bccbe4e56bcc6ac9b46d4b545d2243b1de21ce
unknown <dimaslutfhi82@gmail.com>
2020-07-01 18:47:48 +0700

    adm lg

--- resources/views/app/admin/app_dash/status_order.blade.php
+++ resources/views/app/admin/app_dash/status_order.blade.php
@@ -20,7 +20,8 @@
                                     <th>Address</th>
                                     <th>Phone Number</th>
                                     <th>Status</th>
-                                    <th></th>
+                                    <th>Update</th>
+                                    <th>Delete</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -45,7 +46,9 @@
                                 </td>
                                 <td>
                                     <button type="submit" id="cf-submit" name="submit" class="btn btn-primary"><i class="fa fa-refresh"></i> Update</>
-                                        <a href="{{ url('admin/status_order/'.$order->order_id.'/delete') }}" class="btn btn-danger"><i class="fas fa-trash"></i></a>
+                                </td>
+                                <td>
+                                    <a href="{{ url('admin/status_order/'.$order->order_id.'/delete') }}" class="btn btn-danger"><i class="fas fa-trash"></i></a>
                                 </td>
                                 </form>
                                 </tr>

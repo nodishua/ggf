55a8aedaf8e5c205e1e7ead5e383a1deec76a059
unknown <dimaslutfhi82@gmail.com>
2020-07-11 19:51:11 +0700

    110720

--- resources/views/app/user/app_dash/how_to_order.blade.php
+++ resources/views/app/user/app_dash/how_to_order.blade.php
@@ -5,11 +5,11 @@
             <div class="text-center img-center mt-3 ml-3 mr-3">
                 <hr>
                 <strong> 1. Pilih Product Yang Anda Inginkan dan Tekan Order.</strong><hr>
-                <img src="{{ URL::asset('/style/front/img/step1.png') }}" class="img-fluid w-100 d-block"><hr>
+                <img src="{{ URL::asset('/style/front/img/step1.PNG') }}" class="img-fluid w-100 d-block"><hr>
                 <strong> 2. Masukkan Jumlah / Quantity Barang yang Akan Dibeli,
                 Untuk Pembelian Spray Paint Diharapkan Memberikan Note Untuk Warna Yang Dipilih. Add To Cart. </strong>
                 <hr>
-                <img src="{{ URL::asset('/style/front/img/step2.png') }}" class="img-fluid w-100 d-block">
+                <img src="{{ URL::asset('/style/front/img/step2.PNG') }}" class="img-fluid w-100 d-block">
                 <div class="text-center mt-3">
                     <strong>List Warna : </strong>
                     <a href="{{ route('spray300ml') }}" class="btn btn-outline-dark"><span>Ukuran 300ML</span></a>
@@ -17,13 +17,13 @@
                 </div>
                 <hr>
                 <strong> 3. Lakukan Checkout, Sebelumnya Pembeli Diharapkan Mengisi Semua Identitas Keperluan Untuk Pengiriman</strong><hr>
-                <img src="{{ URL::asset('/style/front/img/step3.png') }}" class="img-fluid w-100 d-block"><br>
+                <img src="{{ URL::asset('/style/front/img/step3.PNG') }}" class="img-fluid w-100 d-block"><br>
                 <hr>
                 <strong> 4. Jika Sudah Pembeli Akan Diberikan Form Pembayaran Dengan Kode Unik Masing-Masing Untuk Mempermudah Pengecekan Dana Pembelian Yang Masuk</strong><hr>
-                <img src="{{ URL::asset('/style/front/img/step4.png') }}" class="img-fluid w-100 d-block"><br>
+                <img src="{{ URL::asset('/style/front/img/step4.PNG') }}" class="img-fluid w-100 d-block"><br>
                 <hr>
                 <strong> 5. Pembeli Dapat Memeriksa Riwayat Pembelian Sekaligus Memeriksa Status Pemesanan</strong><hr>
-                <img src="{{ URL::asset('/style/front/img/step5.png') }}" class="img-fluid w-100 d-block"><br>
+                <img src="{{ URL::asset('/style/front/img/step5.PNG') }}" class="img-fluid w-100 d-block"><br>
             </div>
         </div>
     </div>
